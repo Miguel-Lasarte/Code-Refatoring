d52847ed7e52353346d4e463252805f9a6b8bb53
Miguel-Lasarte <mibu3949@student.uu.se>
2026-01-14 14:37:53 +0100

    Breakdown Game file into Game, Player ,Entity and Background Files

---
+++ Source/Background.cpp
@@ -0,0 +1,50 @@
+#include "game.h"
+
+Star::Star(Vector2 pos, float size) : initPosition(pos), position(pos), size(size) {
+}
+
+Background::Background() {
+	stars.reserve(GameConstants::Background::STAR_COUNT);
+
+	for (int i = 0; i < GameConstants::Background::STAR_COUNT; ++i) {
+		const Vector2 pos = {
+			static_cast<float>(GetRandomValue(-GameConstants::Background::Stars::SPAWN_OFFSET, GetScreenWidth() + GameConstants::Background::Stars::SPAWN_OFFSET)),
+			static_cast<float>(GetRandomValue(0,GetRenderHeight()))
+		};
+		const float size = static_cast<float>(GetRandomValue(GameConstants::Background::Stars::MIN_SIZE, GameConstants::Background::Stars::MAX_SIZE)) / GameConstants::Background::Stars::SIZE_DIVISOR;
+		stars.emplace_back(pos, size);
+	}
+}
+void Star::Update(float offset)
+{
+	position.x = initPosition.x + offset;
+	position.y = initPosition.y;
+
+}
+
+void Star::Render() const
+{
+	DrawCircle(
+		static_cast<int>(position.x),
+		static_cast<int>(position.y),
+		size,
+		color
+	);
+}
+
+void Background::Update(float offset)
+{
+	for (auto& star : stars)
+	{
+		star.Update(offset);
+	}
+
+}
+
+void Background::Render() const
+{
+	for (const auto& star : stars)
+	{
+		star.Render();
+	}
+}
---
+++ Source/Player.cpp
@@ -0,0 +1,49 @@
+#include "game.h"
+
+Player::Player(float screenWidth) : xPos(screenWidth / 2.0f) {
+}
+
+void Player::Update()
+{
+	using namespace GameConstants::Player;
+	direction = 0;
+	if (IsKeyDown(KEY_LEFT)) direction--;
+
+	if (IsKeyDown(KEY_RIGHT)) direction++;
+
+	xPos += SPEED * static_cast<float>(direction);
+
+	const float minX = RADIUS;
+	const float maxX = static_cast<float>(GetScreenWidth()) - RADIUS;
+
+	if (xPos < minX) xPos = minX;
+	else if (xPos > maxX) xPos = maxX;
+
+	timer += GetFrameTime();
+	if (timer >= Animation::INTERVAL)
+	{
+		activeTexture = (activeTexture + 1) % Rendering::TEXTURE_COUNT;
+		timer = 0.f;
+	}
+}
+
+void Player::Render(const Resources& resources) const
+{
+	using namespace GameConstants::Player;
+	float window_height = static_cast<float>(GetScreenHeight());
+
+	DrawTexturePro(resources.GetShipTexture(activeTexture),
+		{
+			0,
+			0,
+			Rendering::SOURCE_SIZE,
+			Rendering::SOURCE_SIZE,
+		},
+		{
+			xPos, window_height - BASE_HEIGHT,
+			Rendering::TEXTURE_SIZE,
+			Rendering::TEXTURE_SIZE,
+		}, { Rendering::TEXTURE_OFFSET , Rendering::TEXTURE_OFFSET },
+		0,
+		WHITE);
+}
