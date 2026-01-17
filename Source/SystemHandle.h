278ff911f3fdb04a4b7d6e68f8af482ef0ca370a
Miguel-Lasarte <mibu3949@student.uu.se>
2026-01-17 16:42:36 +0100

    Implemente4d string view in System Handle

--- Source/SystemHandle.h
+++ Source/SystemHandle.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "raylib.h"
+#include <string_view>
 
 struct Window {
 
-	Window(int width, int height, const char* title) {
-		InitWindow(width, height, title);
+	Window(int width, int height,std::string_view title ) {
+		InitWindow(width, height, title.data());
+		if(!IsWindowReady()) {
+			throw std::runtime_error("Failed to initialize window");
+		}
 	}
 	~Window() {
 		CloseWindow();
@@ -15,6 +19,9 @@ struct Audio
 {
 	Audio() {
 		InitAudioDevice();
+		if(!IsAudioDeviceReady()) {
+			throw std::runtime_error("Failed to initialize audio device");
+		}
 	}
 	~Audio() {
 		CloseAudioDevice();
